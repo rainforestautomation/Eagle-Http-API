85e86c78be919407050066feb6865f51e7934c19
Rainforest Automation <john.lee@rainforestautomation.com>
2015-03-17 16:34:37 -0700

    Update api_classes.py

--- api_classes.py
+++ api_classes.py
@@ -6,7 +6,7 @@ from base_class import BaseCluster as NetworkInfo
 from base_class import BaseCluster as PriceCluster
 from base_class import BaseCluster as DeviceInfo
 from base_class import BaseCluster as MessageCluster
-from base_class import BaseCluster as CurrentSummationDelivered
+from base_class import BaseCluster as CurrentSummation
 from base_class import BaseCluster as ScheduleInfo
 from base_class import BaseCluster as BlockPriceDetail
 from base_class import BaseCluster as HistoryData
